808bccc64e8a0eb14fe051e723be255884d30988
Senthuran100 <senthuran.manoharan@gmail.com>
2022-11-18 08:31:55 +0800

    Add insert records

--- queries.sql
+++ queries.sql
@@ -34,6 +34,29 @@ constraint fk_order_id foreign key (order_id) references orders (ID),
 constraint fk_item_id  foreign key (item_id) references products (ID)
 )
 
+use order_details
+
+insert into customers (`Name`,`Description`) values ('John Doe','sample customers')
+insert into customers (`Name`,`Description`) values ('customer 2','sample customers')
+insert into customers (`Name`,`Description`) values ('customer 3','sample customers')
+
+insert into products (`Name`,`Price`,`Description`) values ('product1',10,'sample product')
+insert into products (`Name`,`Price`,`Description`) values ('product2',12.50,'sample product')
+insert into products (`Name`,`Price`,`Description`) values ('product3',100.25,'sample product')
+
+insert into orders (`Date`,`customer_id`,`status`) values (CURDATE(),1,1)
+insert into orders (`Date`,`customer_id`,`status`) values (CURDATE(),1,1)
+insert into orders (`Date`,`customer_id`,`status`) values (CURDATE(),1,2)
+insert into orders (`Date`,`customer_id`,`status`) values (CURDATE(),2,1)
+insert into orders (`Date`,`customer_id`,`status`) values (CURDATE(),3,2)
+insert into orders (`Date`,`customer_id`,`status`) values (CURDATE(),2,2)
+
+
+insert into orders_item (`order_id`,`item_id`,`quantity`,`price`) values (1,1,3,2)
+insert into orders_item (`order_id`,`item_id`,`quantity`,`price`) values (1,2,2,2)
+insert into orders_item (`order_id`,`item_id`,`quantity`,`price`) values (2,3,2,2)
+insert into orders_item (`order_id`,`item_id`,`quantity`,`price`) values (3,3,4,2)
+
 
 
 
