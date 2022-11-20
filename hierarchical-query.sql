afed375cc1691a4c06d9c0f35f28632c1d51aa18
Senthuran100 <senthuran.manoharan@gmail.com>
2022-11-20 13:31:37 +0800

    Add hierarchical data

---
+++ hierarchical-query.sql
@@ -0,0 +1,47 @@
+
+-- Create a category table
+CREATE TABLE category (
+  id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  title varchar(255) NOT NULL,
+  parent_id int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (parent_id) REFERENCES category (id) 
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Inserting data into category table.
+INSERT INTO category(title,parent_id) 
+VALUES('Electronics',NULL);
+
+INSERT INTO category(title,parent_id) 
+VALUES('Laptops & PC',1);
+
+
+INSERT INTO category(title,parent_id) 
+VALUES('Laptops',2);
+INSERT INTO category(title,parent_id) 
+VALUES('PC',2);
+
+INSERT INTO category(title,parent_id) 
+VALUES('Cameras & photo',1);
+INSERT INTO category(title,parent_id) 
+VALUES('Camera',5);
+
+INSERT INTO category(title,parent_id) 
+VALUES('Phones & Accessories',1);
+INSERT INTO category(title,parent_id) 
+VALUES('Smartphones',7);
+
+INSERT INTO category(title,parent_id) 
+VALUES('Android',8);
+INSERT INTO category(title,parent_id) 
+VALUES('iOS',8);
+INSERT INTO category(title,parent_id) 
+VALUES('Other Smartphones',8);
+
+INSERT INTO category(title,parent_id) 
+VALUES('Batteries',7);
+INSERT INTO category(title,parent_id) 
+VALUES('Headsets',7);
+INSERT INTO category(title,parent_id) 
+VALUES('Screen Protectors',7);
